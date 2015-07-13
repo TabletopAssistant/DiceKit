0c58e9709c9985c6b85c099293cad009484c37ad
Brentley Jones <github@brentleyjones.com>
2015-07-13 09:12:14 -0500

    Initial commit

---
+++ DiceKit/DiceKit.h
@@ -0,0 +1,15 @@
+//
+//  DiceKit.h
+//  DiceKit
+//
+//  Created by Brentley Jones on 7/12/15.
+//  Copyright © 2015 Brentley Jones. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DiceKit.
+FOUNDATION_EXPORT double DiceKitVersionNumber;
+
+//! Project version string for DiceKit.
+FOUNDATION_EXPORT const unsigned char DiceKitVersionString[];
