b06124e0a10a3ca4136355777adecca6626761da
Brentley Jones <github@brentleyjones.com>
2015-07-25 00:41:15 -0500

    Add ProbabilityMass type
    Also add ApproximatelyEquatable protocol for comparing with a delta.

---
+++ DiceKit/ProbabilityMass.swift
@@ -0,0 +1,150 @@
+//
+//  ProbabilityMass.swift
+//  DiceKit
+//
+//  Created by Brentley Jones on 7/19/15.
+//  Copyright © 2015 Brentley Jones. All rights reserved.
+//
+
+import Foundation
+
+public struct ProbabilityMass: ApproximatelyEquatable {
+    
+    public typealias Value = FrequencyDistribution.Value
+    public typealias Probability = Double
+    
+    public static let defaultProbabilityEqualityDelta = 1e-16 // This is for 64-bit. What should 32-bit be?
+    public static var probabilityEqualityDelta = defaultProbabilityEqualityDelta
+    
+    public static let zero = ProbabilityMass(FrequencyDistribution.additiveIdentity)
+    
+    public let frequencyDistribution: FrequencyDistribution
+    
+    internal init(_ frequencyDistribution: FrequencyDistribution, normalize: Bool) {
+        self.frequencyDistribution = normalize ? frequencyDistribution.normalizeOutcomes() : frequencyDistribution
+    }
+    
+    public init(_ frequencyDistribution: FrequencyDistribution) {
+        self.init(frequencyDistribution, normalize: true)
+    }
+    
+    public init(_ constant: Int) {
+        self.init(FrequencyDistribution([constant : 1]), normalize: false)
+    }
+    
+}
+
+// MARK: - CustomStringConvertible
+
+extension ProbabilityMass: CustomStringConvertible {
+    
+    public var description: String {
+        return frequencyDistribution.description
+    }
+    
+}
+
+// MARK: - Equatable
+
+public func == (lhs: ProbabilityMass, rhs: ProbabilityMass) -> Bool {
+    return lhs.frequencyDistribution == rhs.frequencyDistribution
+}
+
+// MARK: - ApproximatelyEquatable
+
+public func ≈ (lhs: ProbabilityMass, rhs: ProbabilityMass) -> Bool {
+    return lhs.frequencyDistribution.approximatelyEqual(rhs.frequencyDistribution, delta: ProbabilityMass.probabilityEqualityDelta)
+}
+
+// MARK: - Indexable
+
+extension ProbabilityMass: Indexable {
+    
+    public typealias Index = FrequencyDistributionIndex
+    public typealias _Element = (Value, Probability)
+    
+    /// Returns the `Index` for the given value, or `nil` if the value is not
+    /// present in the frequency distribution.
+    public func indexForValue(value: Value) -> Index? {
+        return frequencyDistribution.indexForValue(value)
+    }
+    
+    public var startIndex: Index {
+        return frequencyDistribution.startIndex
+    }
+    
+    public var endIndex: Index {
+        return frequencyDistribution.endIndex
+    }
+    
+    public subscript(index: Index) -> (Value, Probability) {
+        return frequencyDistribution[index]
+    }
+    
+}
+
+// MARK: - CollectionType
+
+extension ProbabilityMass: CollectionType {
+    // Protocol defaults cover implmentation after conforming to `Indexable`
+}
+
+// MARK: - Operations
+
+extension ProbabilityMass {
+    
+    public subscript(value: Value) -> Probability? {
+        get {
+            return frequencyDistribution[value]
+        }
+    }
+    
+    public func negate() -> ProbabilityMass {
+        let freqDist = frequencyDistribution.negateValues()
+        
+        return ProbabilityMass(freqDist, normalize: false)
+    }
+    
+    public func xor(x: ProbabilityMass) -> ProbabilityMass {
+        let freqDist = frequencyDistribution.add(x.frequencyDistribution)
+        
+        return ProbabilityMass(freqDist, normalize: true)
+    }
+    
+    public func and(x: ProbabilityMass) -> ProbabilityMass {
+        let freqDist = frequencyDistribution.multiply(x.frequencyDistribution)
+        
+        return ProbabilityMass(freqDist, normalize: true)
+    }
+    
+    public func product(x: ProbabilityMass) -> ProbabilityMass {
+        let freqDist = frequencyDistribution.power(x.frequencyDistribution)
+        
+        return ProbabilityMass(freqDist, normalize: true)
+    }
+    
+}
+
+// MARK: - Operators
+
+public prefix func - (x: ProbabilityMass) -> ProbabilityMass {
+    return x.negate()
+}
+
+// Logical XOR
+infix operator ^^ {
+    associativity left
+    precedence 110
+}
+
+public func ^^ (lhs: ProbabilityMass, rhs: ProbabilityMass) -> ProbabilityMass {
+    return lhs.xor(rhs)
+}
+
+public func && (lhs: ProbabilityMass, rhs: ProbabilityMass) -> ProbabilityMass {
+    return lhs.and(rhs)
+}
+
+public func * (lhs: ProbabilityMass, rhs: ProbabilityMass) -> ProbabilityMass {
+    return lhs.product(rhs)
+}
---
+++ DiceKitTests/ProbabilityMass_Tests.swift
@@ -0,0 +1,174 @@
+//
+//  ProbabilityMass_Tests.swift
+//  DiceKit
+//
+//  Created by Brentley Jones on 7/25/15.
+//  Copyright © 2015 Brentley Jones. All rights reserved.
+//
+
+import XCTest
+import Nimble
+
+import DiceKit
+
+/// Tests the `ProbabilityMass` type
+class ProbabilityMass_Tests: XCTestCase {
+    
+    let operationFixture1 = ProbabilityMass(FrequencyDistribution([1: 2.0, 4: 3.0, 11: 1.0]))
+    let operationFixture2 = ProbabilityMass(FrequencyDistribution([1: 1.5, 2: 7.0]))
+
+}
+
+// MARK: - init() tests
+extension ProbabilityMass_Tests {
+    
+    func test_init_shouldNormalizeFrequencyDistribution() {
+        let freqDist = FrequencyDistribution([1: 1.0, 2: 6.0, 6: 3.0])
+        let expectedFreqDist = freqDist.normalizeOutcomes()
+        
+        let probMass = ProbabilityMass(freqDist)
+        
+        expect(probMass.frequencyDistribution) == expectedFreqDist
+    }
+    
+    func test_init_shouldWorkWithConstant() {
+        let constant = 8
+        let expectedFreqDist = FrequencyDistribution([constant: 1.0])
+        
+        let probMass = ProbabilityMass(constant)
+        
+        expect(probMass.frequencyDistribution) == expectedFreqDist
+    }
+    
+}
+
+// MARK: - Equatable
+extension ProbabilityMass_Tests {
+    
+    // TODO: Equatable
+    
+}
+
+// MARK: - ApproximatelyEquatable
+extension ProbabilityMass_Tests {
+    
+    func test_approximatelyEqualOperator_shouldUseDelta() {
+        
+        let delta = ProbabilityMass.defaultProbabilityEqualityDelta
+        let probability = 0.5
+        let insideDeltaProbability = probability + delta/10
+        let outsideDeltaProbability = probability + delta
+        let x = ProbabilityMass(FrequencyDistribution([1: probability, 2: 1.0-probability]))
+        let insideDelta = ProbabilityMass(FrequencyDistribution([1: insideDeltaProbability, 2: 1.0-insideDeltaProbability]))
+        let outsideDelta = ProbabilityMass(FrequencyDistribution([1: outsideDeltaProbability, 2: 1.0-outsideDeltaProbability]))
+        
+        let correctlyInside = x ≈ insideDelta
+        let correctlyOutside = !(x ≈ outsideDelta)
+        
+        expect(correctlyInside) == true
+        expect(correctlyOutside) == true
+    }
+    
+}
+
+// MARK: - Operations
+extension ProbabilityMass_Tests {
+    
+    func test_subscript() {
+        let x = operationFixture2
+        // TODO: Make these things enumerable
+        let expectedProbabilities = x.frequencyDistribution.outcomesPerValue.map {
+            (value, outcome) in x.frequencyDistribution[value]!
+        }
+        
+        let probabilities = x.frequencyDistribution.outcomesPerValue.map {
+            (value, outcome) in x[value]!
+        }
+        
+        expect(probabilities) == expectedProbabilities
+    }
+    
+    func test_negate_shouldNegateValuesOfFrequencyDistibution() {
+        let x = operationFixture2
+        let expectedFreqDist = x.frequencyDistribution.negateValues().normalizeOutcomes()
+        
+        let probMass = x.negate()
+        
+        expect(probMass.frequencyDistribution) == expectedFreqDist
+    }
+    
+    func test_xor_shouldAddFrequencyDistributions() {
+        let x = operationFixture1
+        let y = operationFixture1
+        let expectedFreqDist = x.frequencyDistribution.add(y.frequencyDistribution).normalizeOutcomes()
+        
+        let probMass = x.xor(y)
+        
+        expect(probMass.frequencyDistribution) == expectedFreqDist
+    }
+    
+    func test_and_shouldMultiplyFrequencyDistributions() {
+        let x = operationFixture1
+        let y = operationFixture1
+        let expectedFreqDist = x.frequencyDistribution.multiply(y.frequencyDistribution).normalizeOutcomes()
+        
+        let probMass = x.and(y)
+        
+        expect(probMass.frequencyDistribution) == expectedFreqDist
+    }
+    
+    func test_product_shouldPowerFrequerncyDistributions() {
+        let x = operationFixture1
+        let y = operationFixture1
+        let expectedFreqDist = x.frequencyDistribution.power(y.frequencyDistribution).normalizeOutcomes()
+        
+        let probMass = x.product(y)
+        
+        expect(probMass.frequencyDistribution) == expectedFreqDist
+    }
+    
+}
+
+// MARK: - Operators
+extension ProbabilityMass_Tests {
+    
+    func test_negateOperator_callsNegate() {
+        let x = operationFixture1
+        let expectedProbMass = x.negate()
+        
+        let probMass = -x
+        
+        expect(probMass) == expectedProbMass
+    }
+    
+    func test_xorOperator_callsXor() {
+        let x = operationFixture1
+        let y = operationFixture2
+        let expectedProbMass = x.xor(y)
+        
+        let probMass = x ^^ y
+        
+        expect(probMass) == expectedProbMass
+    }
+    
+    func test_andOperator_callsAnd() {
+        let x = operationFixture1
+        let y = operationFixture2
+        let expectedProbMass = x.and(y)
+        
+        let probMass = x && y
+        
+        expect(probMass) == expectedProbMass
+    }
+    
+    func test_productOperator_callsProduct() {
+        let x = operationFixture1
+        let y = operationFixture2
+        let expectedProbMass = x.product(y)
+        
+        let probMass = x * y
+        
+        expect(probMass) == expectedProbMass
+    }
+    
+}
