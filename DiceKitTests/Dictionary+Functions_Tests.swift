2541d08903c9ed13054ab512fa153376ac534564
Brentley Jones <github@brentleyjones.com>
2015-08-08 18:18:50 -0500

    Add conflict resolution dictionary map functions
    Needed to properly support successfulness mapping

--- DiceKitTests/Dictionary+Functions_Tests.swift
+++ DiceKitTests/Dictionary+Functions_Tests.swift
@@ -35,12 +35,34 @@ class Dictionary_Functions_Tests: XCTestCase {
         expect(mappedKeys) == expected
     }
     
+    func test_mapKeysWithConflictResolution() {
+        let original = [2: 6, 3: 4, 7: 9, 64: 5]
+        let expected = [1: 10, 3: 9, 32: 5] // /2
+        
+        let mappedValues = original.mapKeys ({ $1 + $2}) {
+            (key, value) in key / 2
+        }
+        
+        expect(mappedValues) == expected
+    }
+    
     func test_map() {
         let original = [1: 6, 2:  4, 7: 9, 64: 5]
         let expected = [2: 1, 3: -1, 8: 4, 65: 0] // +1 keys, -5 values
         
         let mapped = original.map {
             (key, value) in (key + 1, value - 5)
+        }
+        
+        expect(mapped) == expected
+    }
+    
+    func test_mapWithConflictResolution() {
+        let original = [2: 6, 3: 4, 7: 9, 64: 5]
+        let expected = [1: 0, 3: 4, 32: 0] // /2 keys, -5 values
+        
+        let mapped = original.map ({ $1 + $2 }) {
+            (key, value) in (key/2, value - 5)
         }
         
         expect(mapped) == expected
