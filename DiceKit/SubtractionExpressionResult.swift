da041286cd8188e8c96f298a8ed3ff8b656d2887
Jonathan Hoffman <jonathanghoffman@gmail.com>
2015-08-16 23:30:32 -0500

    Add `SubtractionExpression` type

---
+++ DiceKit/SubtractionExpressionResult.swift
@@ -0,0 +1,58 @@
+//
+//  SubtractionExpressionResult.swift
+//  DiceKit
+//
+//  Created by Developer on 8/15/15.
+//  Copyright © 2015 Brentley Jones. All rights reserved.
+//
+
+import Foundation
+
+public struct SubtractionExpressionResult<MinuendResult: protocol<ExpressionResultType, Equatable>, SubtrahendResult: protocol<ExpressionResultType, Equatable>>: Equatable {
+    
+    public let minuendResult: MinuendResult
+    public let subtrahendResult: SubtrahendResult
+    
+    public init(_ minuendResult: MinuendResult, _ subtrahendResult: SubtrahendResult) {
+        self.minuendResult = minuendResult
+        self.subtrahendResult = subtrahendResult
+    }
+    
+}
+
+// MARK: - CustomStringConvertible
+
+extension SubtractionExpressionResult: CustomStringConvertible {
+    
+    public var description: String {
+        return "\(minuendResult) - \(subtrahendResult)"
+    }
+    
+}
+
+// MARK: - CustomDebugStringConvertible
+
+extension SubtractionExpressionResult: CustomDebugStringConvertible {
+    
+    public var debugDescription: String {
+        return "\(String(reflecting: minuendResult)) - \(String(reflecting: subtrahendResult))"
+    }
+    
+}
+
+// MARK: - Equatable
+
+public func == <L, R>(lhs: SubtractionExpressionResult<L,R>, rhs: SubtractionExpressionResult<L,R>) -> Bool {
+    return lhs.minuendResult == rhs.minuendResult && lhs.subtrahendResult == rhs.subtrahendResult
+}
+
+// MARK: - ExpressionResultType
+
+extension SubtractionExpressionResult: ExpressionResultType {
+    
+    public var value: Int {
+        return minuendResult.value - subtrahendResult.value
+    }
+    
+}
+
---
+++ DiceKitTests/SubtractionExpressionResult_Tests.swift
@@ -0,0 +1,128 @@
+//
+//  SubtractionExpressionResult_Tests.swift
+//  DiceKit
+//
+//  Created by Jonathan Hoffman on 8/15/15.
+//  Copyright © 2015 Brentley Jones. All rights reserved.
+//
+
+import XCTest
+import Nimble
+import SwiftCheck
+
+import DiceKit
+
+/// Tests the `SubtractionExpressionResult` type
+class SubtractionExpressionResult_Tests: XCTestCase {
+    
+}
+
+// MARK: - Equatable
+extension SubtractionExpressionResult_Tests {
+    
+    func test_shouldBeReflexive() {
+        property("reflexive") <- forAll {
+            (a: Constant, b: Constant) in
+            
+            return EquatableTestUtilities.checkReflexive { SubtractionExpressionResult(a, b) }
+        }
+    }
+    
+    func test_shouldBeSymmetric() {
+        property("symmetric") <- forAll {
+            (a: Constant, b: Constant) in
+            
+            return EquatableTestUtilities.checkSymmetric { SubtractionExpressionResult(a, b) }
+        }
+    }
+    
+    func test_shouldBeTransitive() {
+        property("transitive") <- forAll {
+            (a: Constant, b: Constant) in
+            
+            return EquatableTestUtilities.checkTransitive { SubtractionExpressionResult(a, b) }
+        }
+    }
+    
+    func test_shouldBeAbleToNotEquate() {
+        property("non-equal") <- forAll {
+            (a: Constant, b: Constant, m: Constant, n: Constant) in
+            
+            return (!(a == m && b == n) && !(a == n && b == m)) ==> {
+                EquatableTestUtilities.checkNotEquate(
+                    { SubtractionExpressionResult(a, b) },
+                    { SubtractionExpressionResult(m, n) }
+                )
+            }
+        }
+    }
+    
+    func test_shouldBeAnticommutative() {
+        property("Anticommutative") <- forAll {
+            (a: Constant, b: Constant) in
+            
+            return (a != b) ==> {
+                let x = SubtractionExpression(a, b).evaluate()
+                let y = SubtractionExpression(b, a).evaluate()
+                
+                let testNoncommutative = x != y
+                let testAnticommutative = x.value == -y.value
+                
+                return testNoncommutative && testAnticommutative
+            }
+        }
+    }
+    
+}
+
+// MARK: - ExpressionResultType
+extension SubtractionExpressionResult_Tests {
+    
+    func test_value_shouldSubtractSubtrahendFromMinuend() {
+        property("Subtract subtrahend from minuend") <- forAll {
+            (a: Int16, b: Int16) in
+            
+            let minuend = Int(a)
+            let subtrahend = Int(b)
+            
+            let expectedValue = minuend - subtrahend
+            
+            let result = SubtractionExpressionResult(c(minuend), c(subtrahend))
+            
+            let value = result.value
+            
+            return value == expectedValue
+        }
+    }
+    
+}
+
+// MARK: - CustomDebugStringConvertible
+extension SubtractionExpressionResult_Tests {
+    
+    func test_CustomDebuStringConvertible() {
+        let expression = d(10) - 5
+        let evaluation = expression.evaluate()
+        let expected = "Die(10).Roll(\(evaluation.minuendResult.value)) - Constant(5)"
+        
+        let result = String(reflecting: evaluation)
+        
+        expect(result) == expected
+    }
+    
+}
+
+// Mark: - CustomStringConvertible
+extension SubtractionExpressionResult_Tests {
+    
+    func test_CustomStringConvertible() {
+        let expression = d(10) - 5
+        let evaluation = expression.evaluate()
+        let expected = "\(evaluation.minuendResult.value)|10 - 5"
+        
+        let result = String(evaluation)
+        
+        expect(result) == expected
+    }
+    
+}
